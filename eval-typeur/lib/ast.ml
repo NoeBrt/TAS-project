0751d19648d8bb867865960db834d72bde414bd3
Noebrt <noe.breton@gmail.com>
2025-11-29 17:17:03 +0100

    add new pterm and type 3.1 and 3.3

--- eval-typeur/lib/ast.ml
+++ eval-typeur/lib/ast.ml
@@ -3,9 +3,24 @@ type pterm =  Var of string
               | App of pterm * pterm
               | N of int
               | Add of pterm * pterm
+              | Sub of pterm * pterm
+              | Cons of pterm * pterm
+              | Head of pterm
+              | Tail of pterm
+              | IfZero of pterm * pterm * pterm
+              | IfEmpty of pterm * pterm * pterm
+              | Fix of pterm
+              | Nil
+              | Let of string * pterm * pterm
 
-type ptype = Var of string
-            | Arr of ptype * ptype | Nat
+type ptype =
+  | Var of string
+  | Arr of ptype * ptype
+  | Nat
+  | List of ptype
+  | Forall of string * ptype
+
+
 
 
 type env = (string * ptype) list
--- eval-typeur/lib/printer.ml
+++ eval-typeur/lib/printer.ml
@@ -6,14 +6,28 @@ let rec print_term(t:pterm) : string =
   |App (t1,t2) -> "("^print_term(t1)^" "^print_term(t2)^")"
   |Abs (x,t) -> "(λ"^x^"."^print_term(t)^")"
   |N n -> string_of_int n
-  |Add (t1,t2) -> "("^print_term(t1)^"+"^print_term(t2)^")";;
+  |Add (t1,t2) -> "("^print_term(t1)^"+"^print_term(t2)^")"
+  |Sub (t1,t2) -> "("^print_term(t1)^"-"^print_term(t2)^")"
+  |Cons (t1,t2) -> "("^print_term(t1)^"::"^print_term(t2)^")"
+  |Head t1 -> "head "^print_term(t1)
+  |Tail t1 -> "tail "^print_term(t1)
+  |IfZero (t1,t2,t3) -> "if zero "^print_term(t1)^" then "^print_term(t2)^" else "^print_term(t3)
+  |IfEmpty (t1,t2,t3) -> "if empty "^print_term(t1)^" then "^print_term(t2)^" else "^print_term(t3)
+  |Fix t1 -> "fix "^print_term(t1)
+  |Nil -> "nil"
+  |Let (x,t1,t2) -> "let "^x^"="^print_term(t1)^" in "^print_term(t2);;
+
 
 
 let rec print_type(t:ptype): string =
   match t with
   Var x ->x
   |Arr (t1,t2) -> "("^print_type(t1)^"->"^print_type(t2)^")"
-  |Nat -> "Nat";;
+  |Nat -> "Nat"
+  |List t1 -> "List "^print_type(t1)
+  |Forall (x,t1) -> "Forall "^x^"."^print_type(t1);;
+
+
 
 let rec string_of_pterm (t : pterm) : string =
   match t with
@@ -25,7 +39,25 @@ let rec string_of_pterm (t : pterm) : string =
   | N n ->
       "N " ^ string_of_int n
   | Add (t1, t2) ->
-      "Add (" ^ string_of_pterm t1 ^ ", " ^ string_of_pterm t2 ^ ")";;
+      "Add (" ^ string_of_pterm t1 ^ ", " ^ string_of_pterm t2 ^ ")"
+  | Sub (t1, t2) ->
+      "Sub (" ^ string_of_pterm t1 ^ ", " ^ string_of_pterm t2 ^ ")"
+  | Cons (t1, t2) ->
+      "Cons (" ^ string_of_pterm t1 ^ ", " ^ string_of_pterm t2 ^ ")"
+  | Head t1 ->
+      "Head (" ^ string_of_pterm t1 ^ ")"
+  | Tail t1 ->
+      "Tail (" ^ string_of_pterm t1 ^ ")"
+  | IfZero (t1, t2, t3) ->
+      "IfZero (" ^ string_of_pterm t1 ^ ", " ^ string_of_pterm t2 ^ ", " ^ string_of_pterm t3 ^ ")"
+  | IfEmpty (t1, t2, t3) ->
+      "IfEmpty (" ^ string_of_pterm t1 ^ ", " ^ string_of_pterm t2 ^ ", " ^ string_of_pterm t3 ^ ")"
+  | Fix t1 ->
+      "Fix (" ^ string_of_pterm t1 ^ ")"
+  | Nil ->
+      "Nil"
+  | Let (x, t1, t2) ->
+      "Let (\"" ^ x ^ "\", " ^ string_of_pterm t1 ^ ", " ^ string_of_pterm t2 ^ ")";;
 
 
 
